92fb549f90dca4261da0a895e8d9e1b51b9b696e
ajay22wwss <140741680+ajay22wwss@users.noreply.github.com>
2023-08-08 18:41:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@
 import streamlit 
 streamlit.title('my parents are beauty')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 & Blueberry Oatmeal')
+streamlit.text('kale,spinach & rocket smoothie')
+streamlit.text('hard-boiled free-range egg')
+
 
